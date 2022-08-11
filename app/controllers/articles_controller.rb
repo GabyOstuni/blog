74d52eb55451b2ea75e147e4b3874acc105b621b
GabyOstuni <gostuni@gmail.com>
2022-08-11 14:40:41 -0300

    Creación del controlador y la vista

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,11 @@
+class ArticlesController < ApplicationController
+  def new
+		@article = Article.new
+		@article.title = "demo"
+  end
+
+  def create
+		@article = Article.create(title: params[:article][:title])
+		render json: @article
+  end
+end
